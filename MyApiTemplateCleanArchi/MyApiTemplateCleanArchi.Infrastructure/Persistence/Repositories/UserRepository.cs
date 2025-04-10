5d1ec59f6e03203505e9ac1c12a4ad178ccdaea0
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2025-04-10 10:32:21 +0200

    test CI/CD

--- MyApiTemplateCleanArchi/MyApiTemplateCleanArchi.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ MyApiTemplateCleanArchi/MyApiTemplateCleanArchi.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -19,11 +19,13 @@ namespace MyApiTemplateCleanArchi.Infrastructure.Persistence.Repositories
             _dbContext = dbContext;
         }
 
+
         public async Task AddAsync(User user)
         {
             _dbContext.Users.Add(user);
             await _dbContext.SaveChangesAsync();
         }
+
 
         public async Task<User> GetByIdAsync(int id)
         {
@@ -34,6 +36,7 @@ namespace MyApiTemplateCleanArchi.Infrastructure.Persistence.Repositories
         {
             return await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == username);
         }
+
 
         public async Task<PagedList<User>> GetAllUsersAsync(PaginationParameters parameters)
         {
